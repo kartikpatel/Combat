33567c7f7acac35a53a5e41098d8562d44dcfdf1
Kartik Patel <kartik.patel@asynchrony.com>
2017-05-30 07:30:48 -0400

    Adding emitter for stars

--- Combat.playground/Contents.swift
+++ Combat.playground/Contents.swift
@@ -9,10 +9,27 @@ let midPoint = CGPoint(x: frame.size.width / 2.0, y: frame.size.height / 2.0)
 
 var scene = SKScene(size: frame.size)
 
+let emitter = SKEmitterNode()
+emitter.particleLifetime = 40
+emitter.particleBlendMode = SKBlendMode.alpha
+emitter.particleBirthRate = 3
+emitter.particleSize = CGSize(width: 4,height: 4)
+emitter.particleColor = SKColor(red: 100, green: 100, blue: 255, alpha: 1)
+emitter.position = CGPoint(x:frame.size.width,y:midPoint.y)
+emitter.particleSpeed = 16
+emitter.particleSpeedRange = 100
+emitter.particlePositionRange = CGVector(dx: 0, dy: frame.size.height)
+emitter.emissionAngle = 3.14
+emitter.advanceSimulationTime(40)
+emitter.particleAlpha = 0.5
+emitter.particleAlphaRange = 0.5
+emitter.zPosition = 1
+scene.addChild(emitter)
+
 let spaceship = SKSpriteNode(imageNamed: "Spaceship")
 spaceship.position = midPoint
 spaceship.setScale(0.1)
-
+spaceship.zPosition = 2
 scene.addChild(spaceship)
 
 spaceship.run(
@@ -22,6 +39,8 @@ spaceship.run(
 )
 
 let view = SKView(frame: frame)
+view.showsFPS = true
+view.showsNodeCount = true
 view.presentScene(scene)
 
 PlaygroundPage.current.liveView = view
