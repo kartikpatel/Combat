ce0390a2aefff9c6d2ae2667aa38ff28b8003d09
Kartik Patel <kartik.patel@asynchrony.com>
2017-05-30 07:18:52 -0400

    Adding spaceship to scene

--- Combat.playground/Contents.swift
+++ Combat.playground/Contents.swift
@@ -5,8 +5,15 @@ import SpriteKit
 import PlaygroundSupport
 
 let frame = CGRect(x: 0, y: 0, width: 320, height: 256)
+let midPoint = CGPoint(x: frame.size.width / 2.0, y: frame.size.height / 2.0)
 
 var scene = SKScene(size: frame.size)
+
+let spaceship = SKSpriteNode(imageNamed: "Spaceship")
+spaceship.position = midPoint
+spaceship.setScale(0.1)
+
+scene.addChild(spaceship)
 
 let view = SKView(frame: frame)
 view.presentScene(scene)
