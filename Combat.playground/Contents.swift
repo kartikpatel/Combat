d1a9b9c5ea36bcbc3cafd311e7917c6f606ba395
Kartik Patel <kartik.patel@asynchrony.com>
2017-05-30 07:11:12 -0400

    Adding SKScene to playground

--- Combat.playground/Contents.swift
+++ Combat.playground/Contents.swift
@@ -1,5 +1,14 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import SpriteKit
+import PlaygroundSupport
 
-var str = "Hello, playground"
+let frame = CGRect(x: 0, y: 0, width: 320, height: 256)
+
+var scene = SKScene(size: frame.size)
+
+let view = SKView(frame: frame)
+view.presentScene(scene)
+
+PlaygroundPage.current.liveView = view
